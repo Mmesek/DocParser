059ed6425cee2da1f6178d6ef642eef281cfd027
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-07-27 23:58:53 +0200

    Abstracted into functions

--- json_parser_test.py
+++ json_parser_test.py
@@ -1,40 +1,51 @@
 import json
 #from json_parser2 import generate_function, generate_object, l
 from json_parser2 import *
-with open("results/Channel.json", "r", newline="", encoding="utf-8") as file:
-    j = json.load(file)
 
-s = ""
-e = ""
-routes = []
-for x, i in enumerate(j):
-    if x == 0:
-        for path in i:
-            if "deprecated" in path["name"]:
-                continue
-            e += generate_function(path)
-            routes += [generate_route(path)]
-        r = generate_enum_of_routes(routes)
-    else:
-        if any(j in i.get("name", "") for j in ["Structure","Types", "Flags"]):
-            s += generate_object(i)
-        elif i.get("generated_date", "") != "":
-            import datetime
+def load(name):
+    with open(f"results/{name}.json", "r", newline="", encoding="utf-8") as file:
+        return json.load(file)
 
-            s = l["boilerplate"] + s
-            s = (
-                l["comment"]
-                + "Generated structure from docs at "
-                + i["generated_date"]
-                + "\n"
-                + l["comment"]
-                + "Generated source code at "
-                + datetime.datetime.today().strftime("%H:%M %Y/%m/%d")
-                + "\n"
-                + s
-            )
+def iterate(name):
+    j = load(name)
+    s = ""
+    e = ""
+    routes = []
+    for x, i in enumerate(j):
+        if x == 0:
+            for path in i:
+                if "deprecated" in path["name"]:
+                    continue
+                e += generate_function(path)
+                routes += [generate_route(path)]
+            r = generate_enum_of_routes(routes)
         else:
-            pass
-s += "\n\n" +r + e
-with open("channel." + l["fileExt"], "w", newline="", encoding="utf-8") as file:
-    file.write(s)
+            if any(j in i.get("name", "") for j in ["Structure","Types", "Flags"]):
+                s += generate_object(i)
+            elif i.get("generated_date", "") != "":
+                import datetime
+
+                s = l["boilerplate"] + s
+                s = (
+                    l["comment"]
+                    + "Generated structure from docs at "
+                    + i["generated_date"]
+                    + "\n"
+                    + l["comment"]
+                    + "Generated source code at "
+                    + datetime.datetime.today().strftime("%H:%M %Y/%m/%d")
+                    + "\n"
+                    + s
+                )
+            else:
+                pass
+    s += "\n\n" + r + e
+    save(name, s)
+from utils import check_if_exists
+
+def save(name, s):
+    check_if_exists('results/src')
+    with open('results/src/'+name+"." + l["fileExt"], "w", newline="", encoding="utf-8") as file:
+        file.write(s)
+
+iterate('Channel')
