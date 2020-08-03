24bacf8be9ecbd2f4f51f45633a9563111de08d7
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-08-04 00:50:19 +0200

    Added code if main and additional sub directories

--- json_parser_test.py
+++ json_parser_test.py
@@ -5,7 +5,7 @@ from utils import check_if_exists, args
 syntax, enum, func, types, values, sizes, l, methods = load_language(args()[1])
 
 def load(name):
-    with open(f"results/{name}.json", "r", newline="", encoding="utf-8") as file:
+    with open(f"results/jsons/{name}.json", "r", newline="", encoding="utf-8") as file:
         return json.load(file)
 
 def iterate(name):
@@ -14,7 +14,7 @@ def iterate(name):
     e = ""
     routes = []
     for x, i in enumerate(j):
-        if x == 0:
+        if x == 0 or type(i) == list:
             for path in i:
                 if "deprecated" in path["name"]:
                     continue
@@ -22,7 +22,7 @@ def iterate(name):
                 routes += [generate_route(path)]
             r = generate_enum_of_routes(routes)
         else:
-            if any(j in i.get("name", "") for j in ["Structure","Types", "Flags"]):
+            if any(j in i.get("name", "") for j in ["Structure","Types", "Flags", "Log Events"]):
                 s += generate_object(i)
             elif i.get("generated_date", "") != "":
                 import datetime
@@ -46,8 +46,17 @@ def iterate(name):
 
 
 def save(name, s):
-    check_if_exists('results/src')
-    with open('results/src/'+name+"." + l["fileExt"], "w", newline="", encoding="utf-8") as file:
+    check_if_exists('results/src'+'/'+args()[1])
+    with open('results/src/'+args()[1]+'/'+name+"." + l["fileExt"], "w", newline="", encoding="utf-8") as file:
         file.write(s)
 
-iterate('Channel')
+if __name__ == "__main__":
+    import sys
+    try:
+        iterate(sys.argv[3])
+    except:
+        import glob
+        from os.path import dirname
+        _o = glob.glob(dirname(__file__) + '/results/jsons/**/*', recursive=True)
+        jsons = '- '+'\n- '.join([i.replace('\\', '/').split('/')[-1].split('.')[0] for i in _o])
+        print('\n\nSpecify json to generate from as a third argument\n\tAvailable docs:\n'+jsons)
