e29b4f96c89501ac7208b29dcd08d83cdcc851d0
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-08-04 23:51:16 +0200

    Added main for downloading, parsing and generating

---
+++ main.py
@@ -0,0 +1,28 @@
+from json_parser_test import iterate
+from utils import args, listFiles
+from docs_downloader import download_docs
+from docs_parser import create_json, parse_docs, save_json
+import time
+s = time.time()
+docs_exists = True
+if not docs_exists:
+    download_docs(args()[0])
+    print("Downloading docs...")
+else:
+    print("Docs already exists. Skipping download.")
+
+docs = [i for i in listFiles('docs', True) if '.md' in i]
+total = []
+for doc in docs:
+    total = []
+    print("Parsing", doc)
+    total += create_json(parse_docs(doc))
+    save_json(doc.split('/')[-1].split('\\')[-1].replace('.md', '').strip('_'), total)
+#save_json('Discord', total)
+
+parsed_docs = listFiles('results/jsons')
+for doc in parsed_docs:
+    print("Generating code for", doc)
+    iterate(doc)
+
+print("Done in", time.time() - s)
