9740d6618db9c3f6a8cd3b3be6283a72c4ec567f
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-04-13 00:20:07 +0200

    Add docstring, decorator & class generation. Add Type model. Remove standalone Route model

--- code_gen-mk2/loader.py
+++ code_gen-mk2/loader.py
@@ -1,11 +1,15 @@
 import datetime
 import json
-from models import Parameter, Function, Argument, Class, TEMPLATES
+from models import Object, Parameter, Function, Class, Type, TEMPLATES
 
 objects = []
-objects.append(TEMPLATES.get("comment") + "Generated source code at")
-objects.append(TEMPLATES.get("comment") + f'{datetime.datetime.today().strftime("%H:%M %Y/%m/%d")}')
-objects.append(TEMPLATES.get("newline") * 2)
+with open("code_gen-mk2/boilerplate.py", "r", newline="", encoding="utf-8") as file:
+    objects.append(file.read())
+
+objects.append(
+    TEMPLATES.get("comment") + "Generated source code at " + f'{datetime.datetime.today().strftime("%H:%M %Y/%m/%d")}'
+)
+objects.append("")
 
 
 def to_list(dct, key):
@@ -13,24 +17,64 @@ def to_list(dct, key):
         Parameter(
             name=v.get("name"),
             documentation=v.get("description"),
-            type_=TEMPLATES.get("types", {}).get(v.get("type"), v.get("type")),
+            type_=Type(v.get("type")),
+            value=v.get("default", None),
+            optional=v.get("optional", False),
+            nullable=v.get("nullable", False),
         )
+        if type(v) is dict
+        else Parameter(name=v)
         for v in dct.get(key, [])
     ]
 
 
-def make_function(dct, key, decorators=None, **kwargs):
-    obj = dct.get(key)
+def make_function(obj, name, **kwargs):
+    # obj = dct.get(key)
+    t = Type(obj.get("return_type"))
+    if "path" in obj:
+        q = to_list(obj, "query_parameters")
+        method = obj.get("method")
+        path = obj.get("path")
+        req_permissions = [Parameter(value=v) for v in obj.get("required_permission", [])]
+        decorators = []
+        if req_permissions:
+            decorators.append(Permissions.with_arguments(*req_permissions))
+
+        decorators.append(
+            RouteDecorator.with_arguments(
+                Parameter(name="method", value=method),
+                Parameter(name="path", value=path),
+                Parameter(name="returns", value=t.render(), sane_value=False),
+            )
+        )
+    else:
+        q = []
+        decorators = []
+
+    _params = to_list(obj, "parameters")
+    j = to_list(obj, "json_parameters")
+    j.sort(key=lambda x: (x.optional, False if x.value is None else True))
+    _params.extend(j)
+    _seen = set()
+    params = []
+    for param in _params:
+        if param.name in _seen:
+            continue
+        params.append(param)
+        _seen.add(param.name)
 
     return Function(
-        parameters=to_list(obj, "parameters"),
-        decorators=decorators,
-        return_type=obj.get("return_type"),
+        parameters=params,
+        name=name,
+        return_type=t,
         documentation=obj.get("description"),
+        keyword_only=q,
+        decorators=decorators,
         **kwargs,
     )
 
-with open("code_gen-mk2/test.json", "r", newline="", encoding="utf-8") as file:
+
+with open("results/jsons/discord.json", "r", newline="", encoding="utf-8") as file:
     t = json.load(file)
 
 
@@ -38,32 +82,30 @@ RouteDecorator = Function(name="route")
 Permissions = Function(name="permissions")
 
 for name, obj in t.items():
-    docs = obj.get("description")
+    if type(obj) is not dict:
+        continue
+    objects.append("")
+    docs = obj.get("description", "")
     if "path" in obj:
-        q = to_list(obj, "query_parameters")
-        method = obj.get("method")
-        path = obj.get("path")
-        req_permissions = [Argument(value=v) for v in obj.get("required_permission")]
-        decorators = []
-        if req_permissions:
-            decorators.append(Permissions.with_arguments(*req_permissions))
-
-        decorators.append(
-            RouteDecorator.with_arguments(
-                Argument(name="method", value=method),
-                Argument(name="path", value=path),
-            )
-        )
-        objects.append(make_function(obj, name, name=name, keyword_only=q, decorators=decorators))
+        objects.append(make_function(obj, name))
     else:
         a = to_list(obj, "parameters")
-        methods = [make_function(f) for f in obj.get("methods")]
+        methods = [make_function(obj.get("methods", {}).get(f), f, is_method=True) for f in obj.get("methods", [])]
         objects.append(Class(name, docs, attributes=a, methods=methods))
-    objects.append(TEMPLATES.get("newline") * 2)
+    objects.append("")
+
+rendered = []
+for obj in objects:
+    if isinstance(obj, Object):
+        rendered.append(obj.render())
+    else:
+        rendered.append(obj)
+
 
 def to_file():
-    with open('code.py','w',newline='',encoding='utf-8') as file:
-        file.writelines(objects)
+    with open("code.py", "w", newline="", encoding="utf-8") as file:
+        file.write(TEMPLATES.get("newline").join(rendered))
+
 
 if True:
     to_file()
--- code_gen-mk2/models.py
+++ code_gen-mk2/models.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from re import template
+from textwrap import indent
 from typing import Any, Optional
+from mlib.utils import try_quote, clean
 
 TEMPLATES = {
     "Object": "name={name}, documentation={documentation}",
@@ -18,13 +21,42 @@ TEMPLATES = get_templates()
 
 
 @dataclass
+class Type:
+    type: type
+    key_type: str = None
+    value_type: str = None
+    is_array: bool = False
+    array_size: int = None
+    lowercase: bool = False
+
+    def render(self, optional: bool = False, nullable: bool = False):
+        TYPES = TEMPLATES.get("types", {})
+        _type = TYPES.get(self.type, self.type)
+        if self.is_array:
+            _type = TYPES.get("array", "{type}").format(type=_type, size=self.array_size)
+        if optional:
+            _type = TYPES.get("optional", "{type}").format(type=_type)
+        if nullable:
+            _type = TYPES.get("nullable", "{type}").format(type=_type)
+        return _type
+
+
+@dataclass
 class Object:
     """Base Object metadata"""
 
     name: str
     """Object's name"""
-    documentation: str
+    documentation: str = ""
     """Object's docstring"""
+    lowercase: bool = True
+    sane_value: bool = True
+
+    def __post_init__(self):
+        for field in self.__dict__:
+            value = getattr(self, field)
+            if value is list:
+                self.__setattr__(field, [])
 
     def __str__(self):
         template = TEMPLATES.get(self.__class__.__name__)
@@ -39,83 +71,196 @@ class Object:
             s = template.format_map({k: v for k, v in self.__dict__.items()})
         return s
 
+    def render(self):
+        pass
+
+    def format_docs(self, **documentation):
+        _docs = documentation.get("main")
+        if not _docs:
+            return
+        template = TEMPLATES.get("documentation", {})
+        docstring = TEMPLATES.get("docstring", "")  # TODO Move
+        INDENT = TEMPLATES.get("indent") * (2 if getattr(self, "is_method", False) else 1)
+        docs = indent(TEMPLATES.get("newline").join(_docs).strip(), INDENT)
+        if docs.strip():
+            return docstring.format(documentation=docs, indent=INDENT, newline=TEMPLATES.get("newline"))
+
+    def format(self, template, **d):
+        if not self.name:
+            return ""
+
+        d.update(
+            {
+                "name": clean(self.name),
+                "newline": TEMPLATES.get("newline"),
+                "documentation": self.format_docs(main=self.documentation),
+                "indent": TEMPLATES.get("indent"),
+            }
+        )
+        if self.sane_value and d.get("value", None) not in [None, "None"]:
+            v = TEMPLATES.get("values", {}).get(d["value"], try_quote(d["value"]))
+            if d["value"] == "absent":
+                breakpoint
+            d["value"] = v  # try_quote(v)
+        if self.lowercase and "name" in d:
+            d["name"] = d["name"].lower()
+        if "Structure" in self.name and not d.get("documentation"):
+            breakpoint
+        if type(template) is list:
+            s = ""
+            for _template in template:
+                try:
+                    s += _template.format_map({k: v for k, v in d.items() if v})
+                except KeyError:
+                    continue
+        else:
+            s = template.format_map({k: v for k, v in d.items()})
+        return s
+
 
 @dataclass
 class Parameter(Object):
     """Parameter metadata"""
 
-    type_: type
+    type_: Type = None
     """Parameter type"""
     optional: bool = False
     """Whether this parameter is optional"""
-    array_size: Optional[int] = 0
+    nullable: bool = False
+    """Whether this parameter is nullable"""
+    # array_size: Optional[int] = 0
     """Array size (If it's an array)"""
-    mapping_type: Optional[tuple[str, str]] = Optional
+    # mapping_type: Optional[tuple[str, str]] = Optional
     """Tuple representing key and value types for dictonaries (maps) or array type and item type (lists or sets)"""
-    default: Optional[Any] = Optional
+    # default: Optional[Any] = Optional
     """Default value"""
+    value: Optional[Any] = None
+    """Argument or Default value"""
+    lowercase: bool = True
 
+    def render(self):
+        template = TEMPLATES.get("parameter")
 
-@dataclass
-class Argument(Parameter):
-    """Argument metadata"""
+        return self.format(template, type=self.type_.render(self.optional) if self.type_ else None, value=self.value)
 
-    value: Any = None
+    def as_argument(self):
+        template = TEMPLATES.get("argument")
+        return self.format(template, value=self.value)
 
 
 @dataclass
 class Function(Object):
     """Function metadata"""
 
-    parameters: list[Parameter]
+    parameters: list[Parameter] = list
     """List of parameters in this function"""
-    keyword_only: list[Parameter]
+    keyword_only: list[Parameter] = list
     """List of keyword-only parameters in this function"""
-    return_type: type
+    return_type: Type = Type(None)
     """Return type"""
     is_method: bool = False
     """Whether it's a method"""
-    decorators: Optional[list["Function"]] = Optional
+    decorators: Optional[list["Function"]] = list
     """List of Functions decorating this function"""
     examples: Optional[str] = Optional
     """Example usage of this function"""
-    arguments: Optional[list[Argument]] = Optional
+    arguments: Optional[list[Parameter]] = list
+    # arguments: Optional[list[Argument]] = Optional
     """Arguments to use with this function"""
+    lowercase: bool = True
 
-    def with_arguments(self, *arguments: Argument) -> "Function":
+    def with_arguments(self, *arguments: Parameter) -> "Function":
         """Returns Function with specified arguments for function call rendering"""
         return Function(name=self.name, arguments=arguments)
+
+    def render(self):
+        template = TEMPLATES.get("function" if not self.is_method else "method", {})
+        decorators = "\n".join([d.as_decorator() for d in self.decorators])
+        decorators = indent(decorators, TEMPLATES.get("indent") * (0 if not self.is_method else 1))
+        parameters = ", ".join([p.render() for p in self.parameters])
+        kwargs = ", ".join([k.render() for k in self.keyword_only])
+        if not kwargs:
+            breakpoint
+        all_params = []
+        if parameters:
+            all_params.append(parameters)
+        if kwargs:
+            all_params.append(TEMPLATES.get("keyword_only"))
+            all_params.append(kwargs)
+        params = ", ".join(all_params)
+        s = self.format(
+            template.get("definition"),
+            async_keyword=TEMPLATES.get("async"),
+            parameters=params,
+            decorators=decorators,
+            return_type=self.return_type.render(),
+        )
+        return s
+
+    def as_call(self):
+        template = TEMPLATES.get("function", {})
+        return self.format(
+            template.get("call", ""), arguments=", ".join([a.as_argument() for a in self.arguments if a.value])
+        )
+
+    def as_decorator(self):
+        template = TEMPLATES.get("decorator", "")
+        return self.format(template, arguments=", ".join([a.as_argument() for a in self.arguments if a.value]))
 
 
 @dataclass
 class Class(Object):
     """Class metadata"""
 
-    bases: list["Class"]
+    bases: list["Class"] = list
     """Types this Class inherits from"""
-    attributes: list[Parameter]
+    attributes: list[Parameter] = list
     """Atributes/Fields of this Class"""
-    methods: Optional[list[Function]] = Optional
+    methods: Optional[list[Function]] = list
     """List of methods attached to this Class"""
     examples: Optional[str] = Optional
     """Example usage of this Class"""
+    lowercase: bool = False
 
+    def render(self):
+        if any(
+            i in self.name.lower()
+            for i in {
+                "type",
+                "enum",
+                "mode",
+                "limits",
+                "level",
+                "tier",
+                "features",
+                "behaviours",
+                "versions",
+                "commands",
+                "events",
+                "opcodes",
+                "url",
+                "scope",
+                "error",
+                "format",
+                "style",
+                "endpoint",
+            }
+        ):
+            _type = "enum"
+        elif "flag" in self.name.lower():
+            _type = "flag"
+        else:  # if "structure" in self.name.lower():
+            # elif any(i in self.name.lower() for i in {"structure", "fields", "object", "response", "params", "properties", "parameters", "activity", "info"}):
+            _type = "class"
+        from mlib.utils import replace_multiple
 
-@dataclass
-class Route(Object):
-    """Route metadata"""
+        self.name = replace_multiple(self.name, TEMPLATES.get("to_strip", []), "").strip()
 
-    method: str
-    """Method used with this Route"""
-    path: str
-    """Path of this Route"""
-    parameters: Optional[list[Parameter]] = Optional
-    """List of JSON parameters for this Route"""
-    query_parameters: Optional[list[Parameter]] = Optional
-    """List of query parameters for this Route"""
-    required_permissions: Optional[list[str]] = Optional
-    """List of required permissions to use this Route"""
-    result_type: Optional[Any] = Optional
-    """Result type of using this Route, if any"""
-    examples: Optional[str] = Optional
-    """Example usage of this Route"""
+        template = TEMPLATES.get(_type, {})
+        attributes = indent("\n".join([a.render() for a in self.attributes]), TEMPLATES.get("indent"))
+        methods = "\n".join([m.render() for m in self.methods])
+        bases = ", ".join(self.bases)
+        s: str = self.format(template.get("definition"), bases=bases, attributes=attributes, methods=methods)
+        if len(set([i.strip() for i in s.splitlines()])) <= 1:
+            return ""
+        return s
